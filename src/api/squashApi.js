6d40e5b84f5b96d41dab8fd8e3c1ead2369a1c88
Filip Tyborowski <Filip.Tyborowski@chrobinson.com>
2019-09-20 14:50:20 +0200

    missing dash

--- src/api/squashApi.js
+++ src/api/squashApi.js
@@ -1,5 +1,5 @@
 import { handleResponse, handleError } from "./apiUtils";
-const baseUrl = process.env.REACT_APP_SQUASH_BASE_URL + "squash/freeCourts";
+const baseUrl = process.env.REACT_APP_SQUASH_BASE_URL + "/squash/freeCourts";
 
 export function getFreeCourts(startDate, startTime, endTime) {
   const request = baseUrl + "/" + startTime + "/" + endTime + "/60";
