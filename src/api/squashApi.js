1d53cce4429789ee04ced6b01f377a0b1ba7da72
Filip Tyborowski <Filip.Tyborowski@chrobinson.com>
2019-09-20 15:27:51 +0200

    ...

--- src/api/squashApi.js
+++ src/api/squashApi.js
@@ -1,5 +1,6 @@
 import { handleResponse, handleError } from "./apiUtils";
-const baseUrl = process.env.REACT_APP_SQUASH_BASE_URL + "/squash/freeCourts";
+// const baseUrl = process.env.REACT_APP_SQUASH_BASE_URL + "/squash/freeCourts";
+const baseUrl = "http://localhost:5000/squash/freeCourts";
 
 export function getFreeCourts(startDate, startTime, endTime) {
   const request = baseUrl + "/" + startTime + "/" + endTime + "/60";
